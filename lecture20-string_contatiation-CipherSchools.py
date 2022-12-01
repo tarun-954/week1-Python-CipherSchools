25df7ff8eee03efab4af5631fef42dc7e1815720
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2022-12-01 23:06:56 +0530

    Add files via upload

---
+++ lecture20-string_contatiation-CipherSchools.py
@@ -0,0 +1,8 @@
+first_name="Tarun"
+last_name="Choudhary"
+full_name=first_name+""+last_name
+print(full_name)
+# print(first_name +3) error
+print(first_name +"3") #no error
+print(first_name+str(3)) #no error
+print(first_name *3)
